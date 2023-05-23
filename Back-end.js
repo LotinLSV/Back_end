112007e18ab9d369c9e13bdb79bf4f89066eb802
lotinlsv <lucasdesouzavaz@hotmail.com>
2023-05-23 17:04:03 -0300

    criação de constantes e estilos de dados JS

---
+++ Back-end.js
@@ -0,0 +1,41 @@
+//  tipos de dados
+
+let x = 4; //numero
+let y = 'teste'; //string
+let z = true; //booleano
+let k = 0.1; //float
+
+
+// dados números
+const meuNumero =  6;
+const segNumero = 7;
+
+const tercNumero = meuNumero + segNumero;
+
+console.log(tercNumero);
+
+console.log(tercNumero + y.length)
+
+const texto1 = "olá mundo";
+const texto2 = 'olá mundo';
+
+console.log (texto1 + " " +texto2);
+
+const number = '\U2705';
+
+
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+const tique = '\u2705';
+const hiragana = '\u3041';
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
+
+console.log("este é um teste " + tique + " você passou!" );
+
+
+console.log(meuNumero === segNumero);
