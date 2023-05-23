a5073a23f6097bca204804af6e1eddd20efabe76
lotinlsv <lucasdesouzavaz@hotmail.com>
2023-05-23 17:28:27 -0300

    continuação

---
+++ Back-end2.js
@@ -0,0 +1,19 @@
+let variavel = null;
+if (variavel == null) {
+    console.log('vazio')
+} else {
+    console.log('com informação')
+}
+
+var altura = 5;
+var comprimento = 7;
+let area;
+
+area = altura * comprimento;
+console.log(area);
+
+// convertendo dados
+
+const numero = 45;
+const numero2 = '45';
+
